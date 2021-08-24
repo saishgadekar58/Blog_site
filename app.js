0d4342312b0a7a8b11e55a65507c53dbec8d7b13
saish gadekar <saish.gadekar.58@gmail.com>
2021-08-24 22:57:54 +0530

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use("/js", express.static(__dirname + "public/js"));
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+const newsRouter = require("./src/routes/news");
+
+app.use("/", newsRouter);
+app.use("/article", newsRouter);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
